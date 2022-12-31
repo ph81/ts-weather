247eef19554fafcaa85d979ab0309fb7be181a9a
Edith M <paperhearts81@gmail.com>
2022-12-31 05:26:46 -0600

    chore: button component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = (): JSX.Element => {
   return (
-    <main className="flex justify-center items-center">
-      <Home />
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
